a447f3bd7183938bfb8111708965141379a7862d
HeyAquarius <heyaquariusai@gmail.com>
2025-06-30 18:29:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
-print("✅ bot.py started")
-
+import os
 import time
+
+with open("log.txt", "w") as f:
+    f.write("✅ bot.py has started!\n")
+    f.flush()
 
 i = 1
 while True:
-    print(f"🔁 Still alive... loop {i}")
+    with open("log.txt", "a") as f:
+        f.write(f"Loop {i}\n")
+        f.flush()
+    print(f"Loop {i}")
     time.sleep(15)
     i += 1
